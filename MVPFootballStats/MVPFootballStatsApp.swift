f271c0d125fd0c13923a8fadd1b42925bda7f4ab
jdenkim <125925293+jdenkim@users.noreply.github.com>
2023-08-28 14:02:12 -0400

    Initial Commit

---
+++ MVPFootballStats/MVPFootballStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPFootballStatsApp.swift
+//  MVPFootballStats
+//
+//  Created by Jden Kim on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MVPFootballStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
